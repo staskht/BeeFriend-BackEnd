99f73173f0784475429144ef1a4eb62205426d8d
staskht <s76579343@gmail.com>
2026-08-22 19:48:05 +0300

    Interests addition to UserProfile

--- BeeFriend.Core/Domain/Entities/Interest.cs
+++ BeeFriend.Core/Domain/Entities/Interest.cs
@@ -6,5 +6,10 @@ namespace BeeFriend.Core.Domain.Entities
 {
     public class Interest
     {
+        public int InterestId { get; set; }
+        public string Name { get; set; } = null!;
+
+        public ICollection<UserProfile> Users = new List<UserProfile>();
+
     }
 }
--- BeeFriend.Core/Domain/Entities/UserProfile.cs
+++ BeeFriend.Core/Domain/Entities/UserProfile.cs
@@ -7,17 +7,13 @@ namespace BeeFriend.Core.Domain.Entities
 {
     public class UserProfile
     {
-        
         public Guid UserId { get; set; }
-
         public ApplicationUser User { get; set; } = null!;
 
         public int? CityId { get; set; }
-
         public City? City { get; set; }
 
         public int? CountryId { get; set; }
-
         public Country? Country { get; set; }
 
         [StringLength(50)]
@@ -27,9 +23,7 @@ namespace BeeFriend.Core.Domain.Entities
         public string? Bio {get; set; }
 
         public DateTime BirthDate { get; set; }
-
         public GenderOptions? Gender {get; set;}
-
         public PronounsOptions? Pronouns {get; set;}
 
         [StringLength(200)]
